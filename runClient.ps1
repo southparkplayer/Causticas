8c041aa28e924390eafb7ac3d4de6af1a5de30c6
ComfyFluffy <24245520+ComfyFluffy@users.noreply.github.com>
2026-07-18 17:36:59 +0900

    update run script

--- runClient.ps1
+++ runClient.ps1
@@ -4,8 +4,8 @@ $candelaRoot = $PSScriptRoot
 
 Push-Location $candelaRoot
 try {
-	$env:JAVA_TOOL_OPTIONS='-Xmx8G -XX:+UseCompactObjectHeaders -XX:+AlwaysPreTouch -XX:+UseStringDeduplication -XX:+UseZGC -Dcandela.rt.fg=false -Dcandela.rt.reflex=false -Dcandela.rt.frameStats=false -Dcandela.rt.lodWorld=true -Dcandela.rt.lodDebug=true'
 	.\gradlew.bat --stop
+	$env:JAVA_TOOL_OPTIONS='-Xmx8G -XX:+UseCompactObjectHeaders -XX:+AlwaysPreTouch -XX:+UseStringDeduplication -XX:+UseZGC'
 	.\gradlew.bat runClient --args="--renderDebugLabels --graphicsBackend VULKAN"
 } finally {
 	Pop-Location
